a432b14b5b62f788ef3798cf55149591903d765b
Tushar R C Raja <startushar.2004@gmail.com>
2025-07-21 13:26:15 +0530

    Create 931-MinimumFallingPathSum.cpp

---
+++ 931-MinimumFallingPathSum.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+public:
+  int f(int i, int j, vector<vector<int>> &arr, int n, int m, vector<vector<int>> &dp)
+  {
+    if (i == n)
+    {
+      return 0;
+    }
+
+    if (j < 0 or j >= m)
+    {
+      return INT_MAX;
+    }
+
+    if (dp[i][j] != INT_MIN)
+    {
+      return dp[i][j];
+    }
+
+    int cur = arr[i][j];
+
+    int a = f(i + 1, j - 1, arr, n, m, dp);
+    int b = f(i + 1, j, arr, n, m, dp);
+    int c = f(i + 1, j + 1, arr, n, m, dp);
+
+    return dp[i][j] = cur + min({a, b, c});
+  }
+  int minFallingPathSum(vector<vector<int>> &arr)
+  {
+
+    int n = arr.size();
+    int m = arr[0].size();
+    int ans = INT_MAX;
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN));
+
+    for (int j = 0; j < m; j++)
+    {
+
+      ans = min(ans, f(0, j, arr, n, m, dp));
+    }
+    return ans;
+  }
+};
